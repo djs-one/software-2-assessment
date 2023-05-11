def73fbc0c849fdfc9ed4b17ef93e8f56c45d4de
dan_stadt <dstadt14@gmail.com>
2023-05-11 13:46:36 -0600

    second save

---
+++ src/controller/loginController.java
@@ -0,0 +1,22 @@
+package controller;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+    protected TextField usernameField;
+    protected TextField passwordField;
+    protected TextField locationField;
+    protected Text usernameText;
+    protected Text passwordText;
+    protected Text locationText;
+    protected Text loginText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resource){
+
+    }
+
+}
