0198537157b76d369c3aebc208161d81a31ad02c
dan_stadt <dstadt14@gmail.com>
2023-05-11 14:52:19 -0600

    second save

--- src/controller/loginController.java
+++ src/controller/loginController.java
@@ -1,22 +1,39 @@
 package controller;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import java.net.URL;
+import java.time.ZoneId;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class loginController implements Initializable {
+    @FXML
     protected TextField usernameField;
+    @FXML
     protected TextField passwordField;
+    @FXML
     protected TextField locationField;
+    @FXML
     protected Text usernameText;
+    @FXML
     protected Text passwordText;
+    @FXML
     protected Text locationText;
+    @FXML
     protected Text loginText;
-
     @Override
     public void initialize(URL location, ResourceBundle resource){
-
+        Locale locale = Locale.getDefault();
+        String language = locale.getLanguage();
+        if (language.equals("fr")){
+            loginText.setText("Connexion");
+            usernameText.setText("Nom d'utilisateur:  ");
+            passwordText.setText("Mot de passe  ");
+            locationField.setText("Emplacement  ");
+        }
+        locationText.setText(ZoneId.systemDefault().getDisplayName(TextStyle.FULL, locale));
     }
-
 }
--- src/model/Main.java
+++ src/model/Main.java
@@ -2,24 +2,22 @@ package model;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.util.Locale;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Inventory - Home");
+        stage.setScene(scene);
+        stage.show();
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
-
 }
